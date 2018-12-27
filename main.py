0a378947b8a4aeea7ca8f58605ce44e4fb810d2a
Patrick Virie <p.virie@gmail.com>
2018-12-27 21:14:49 +0700

    implement cross-correlation conceptor layer

--- main.py
+++ main.py
@@ -14,26 +14,92 @@ data_set = torchvision.datasets.FashionMNIST(os.path.join(root, "data"), train=T
 data_loader = torch.utils.data.DataLoader(data_set, batch_size=1, shuffle=True)
 
 
-num_layers = 5
+class Cross_Correlational_Conceptor:
 
-inputs = data_set[0][0].unsqueeze(1).to(device)
-filters = torch.randn(5, 1, 3, 3, device=device)
-res = torch.nn.functional.conv2d(inputs, filters, padding=1)
+    def __init__(self):
+        print("init")
+        self.weights = []
+        self.new_weights = []
+        self.depth = 0
+        self.kernel_size = (3, 3)
+        self.padding = ((self.kernel_size[0] - 1) // 2, (self.kernel_size[1] - 1) // 2)
+        self.stride = (1, 1)
 
-print(res.shape)
+    def expand(self, expand_depth, include_depth):
+        print("expand")
+        A = torch.randn(expand_depth, include_depth, self.kernel_size[0], self.kernel_size[1], requires_grad=True, device=device)
 
-A = torch.randn(5, 3, 3, 3, device=device)
-B = torch.randn(5, 5, 3, 3, device=device)
+        self.weights.append(A)
+        self.new_weights.append(A)
 
-weights = [A, B]
+    def learn(self, input, expand_depth):
+        print("learn")
+        self.expand(expand_depth, input.shape[1])
 
-res = torch.cat([torch.nn.functional.conv2d(res[:, 0:f.shape[1], ...], f, padding=1) for f in weights], dim=1)
+        optimizer = torch.optim.Adam(self.new_weights, lr=0.001)
+        criterion = torch.nn.MSELoss(reduction='mean')
 
-optimizer = torch.optim.Adam(weights, lr=0.0001)
+        for i in range(20000):
 
-print(res.shape)
+            hidden = self << input
+            input_ = self >> hidden
+            loss = criterion(input_, input)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if i % 1000 == 0:
+                print("step:", i, "th, loss:", loss.item())
+
+        print("final loss:", loss.item())
+        self.new_weights.clear()
+
+    def __lshift__(self, input):
+        res = torch.cat([
+            torch.nn.functional.conv2d(input[:, 0:f.shape[1], ...], f, stride=self.stride, padding=self.padding)
+            for f in self.weights
+        ], dim=1)
+        return res
+
+    # https://github.com/vdumoulin/conv_arithmetic
+    def __rshift__(self, hidden):
+
+        h_out = (hidden.shape[2] - 1) * self.stride[0] - 2 * self.padding[0] + self.kernel_size[0]
+        w_out = (hidden.shape[3] - 1) * self.stride[1] - 2 * self.padding[1] + self.kernel_size[1]
+
+        depth_out = 0
+        for f in self.weights:
+            depth_out = max(depth_out, f.shape[1])
+
+        canvas = torch.zeros([hidden.shape[0], depth_out, h_out, w_out], device=device)
+
+        from_depth = 0
+        for f in self.weights:
+            occupied_depth = f.shape[0]
+            to_depth = from_depth + occupied_depth
+            addition = torch.nn.functional.conv_transpose2d(hidden[:, from_depth:to_depth, ...], f, stride=self.stride, padding=self.padding)
+            canvas[:, 0:occupied_depth, ...] = canvas[:, 0:occupied_depth, ...] + addition
+            from_depth = to_depth
+
+        return canvas
+
 
 if __name__ == "__main__":
     print("main")
 
-    # for i, (data, label) in enumerate(data_loader):
+    layer = Cross_Correlational_Conceptor()
+    criterion = torch.nn.MSELoss(reduction='mean')
+
+    input_0 = data_set[0][0].unsqueeze(1).to(device)
+
+    for i, (data, label) in enumerate(data_loader):
+
+        input = data.to(device)
+        layer.learn(input, 5)
+
+        gen_0 = layer >> (layer << input_0)
+        loss_0 = criterion(gen_0, input_0)
+        print(loss_0.item())
+
+        if i == 9:
+            break
